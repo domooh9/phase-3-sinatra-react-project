4aa78c04496448ff9025d141e3d6e2d5140c9b02
Domnic Omondi <domooh9@gmail.com>
2022-09-10 14:11:29 +0300

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,15 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-
+  post '/users' do
+    user = User.create(
+      name: params[:name],
+      avatar_id: params[:avatar_id],
+      level_id: params[:level_id]
+    )
+    user.to_json
+  end
+  get '/users' do
+    steps.all.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-
+steps={[
+    {
+  id:'intro', 
+     message:'Do you agree to the Terms and Conditions?', 
+     trigger:'intro-user',
+    },
+    {
+id:'intro-user', 
+     options:[
+       {value:'y', label:'Yes', trigger:'yes-response'},
+       {value:'n', label:'No', trigger:'no-response'},
+     ] 
+    },
+    {
+id:'yes-response', 
+     message:'Great!', 
+     end:True,
+    },
+    {
+id:'no-response', 
+     message:'Sorry to hear that.', 
+     end:True,
+    },
 puts "✅ Done seeding!"
